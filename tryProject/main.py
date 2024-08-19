ddd67b07a70ec1d90757cb31debcc70d6c2e7c42
Adonay Rocha <75874305+AdonayRocha@users.noreply.github.com>
2024-08-19 20:40:37 -0300

    Add files via upload

---
+++ tryProject/main.py
@@ -0,0 +1,36 @@
+#Try & Except & Else & Finally
+from datetime import date
+import random
+
+quantidadeTeste = 0
+quantidadeTeste = 0
+
+#Define o número da sorte
+numeroSorte = random.uniform(1, 100)
+print(numeroSorte)
+
+#Número da sorte e tratamento
+while True:
+    numero = input("Insira um número de 1 à 100: ")
+    try:
+        if "," in numero:
+            print("Para especificar números decimais, utilize ponto '.'")
+        numero = float(numero)
+        if 1 <= numero <= 100:
+            print(f"Número válido: {numero}")
+            break
+        else:
+            print("O número deve estar entre 1 e 100.")
+    except ValueError:
+        print("Erro: Entrada inválida.")
+    finally:
+        print("-------")
+        quantidadeTeste += 1
+        dataAtual = date.today()
+        print(f"Log {dataAtual} Tentativa de Input: {quantidadeTeste}")
+        if numero == numeroSorte:
+            print(f"Parabéns, você acertou o número da sorte é: {numeroSorte}")
+        else:
+            print("Não foi dessa vez!")
+        print("-------")
+
